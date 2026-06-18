40c6f8587daaa0443c5603a2ee5a5a2b16aecffb
DreamlessBallpen <107037118+DreamlessBallpen@users.noreply.github.com>
2026-06-19 02:53:13 +0800

    Added my progress on creating a graph object, and added a .gitignore file as well, along with some learnings while doing so.

--- PythonDataStructures/graphs.py
+++ PythonDataStructures/graphs.py
@@ -1,7 +1,8 @@
 # Personal attempt at creating a Graph that can be unidirectional or bi-directional for some items.
 # We are also attempting to make one that can contain weights in the edges, or if not specified, to automatically default to one as the weight value.
 
-# I feel like, due to the unclear set of goals that we have, it's difficulto visualize how to proceed. Let's take a break.
+# I feel like, due to the unclear set of goals that we have, it's difficult to visualize how to proceed.
+# Still, take a break, but don't give up.
 
 class Status:
     def get_status(self):
@@ -17,6 +18,26 @@ class ReturnCode:
 class Graph(Status):
     def __init__(self):
         self.adj_list = {}
+        
+    def __str__(self):
+        # [TESTED]
+        
+        if len(self.adj_list) == 0:
+            return """ 
+            ========= GRAPH =========
+            No vertices in graph.
+            =========================
+            """
+        else:
+            text_to_return = ""
+            
+            text_to_return += "============ GRAPH ============"
+            for k, v in self.adj_list.items():
+                new_line = f"\n[{k}]: {v}"
+                text_to_return += new_line
+            text_to_return += "\n==============================="
+            
+            return text_to_return
     
     def add_vertex(self, v):
         if v in self.adj_list.keys():
@@ -28,13 +49,29 @@ class Graph(Status):
         return ReturnCode.code_dict["00"]
         
     # TODO
-    def add_edge(self, v1, v2, wt=1:float, bidirectional=True:bool, presence_prereq=False:bool):
-        if presence_prereq:
-            # Hard conditions
-            pass
-        else:
-            # Code adjusts when it can
-            pass
+    def add_edge(self, v1, v2, wt:float=1, bidirectional:bool=True, presence_prereq:bool=False):
+        assert wt > 0, "Edge weights cannot be negative."
+        
+        # Compile validation criteria first, make code simpler.
+        v1_and_v2_available = not((not v1 in self.adj_list.keys()) or (not v2 in self.adj_list.keys()))
+        
+        # Evaluate code block "cases" for us to track here.
+        
+        # [CASE1] v1 and v2 exists, bidirectional, presence_prereq is True.
+        
+        # This evaluates what we pass into our 'switch' statement later.
+        code_block_decision = 0
+        if (v1_and_v2_available and bidirectional and presence_prereq):
+            code_block_decision = 1
             
+            
+        # Actual steps.
+        If code_block_decision == 1:
+            # Add the links
+            self.adj_list[v1].append(v2)
+            self.adj_list[v2].append(v1)
+            # paused here, but continue soon!
+            
+        
         
         
